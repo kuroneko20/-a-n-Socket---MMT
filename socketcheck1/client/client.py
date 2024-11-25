955cb5a61473ecec40a7868c38e18cfeb1d0545d
thongpham796 <23120364@student.hcmus.edu.vn>
2024-11-25 22:35:43 +0700

    truyen nhan file

--- socketcheck1/client/client.py
+++ socketcheck1/client/client.py
@@ -4,15 +4,7 @@ HOST = "127.0.0.1"
 # IP = "10.131.3.60"
 SERVER_PORT = 58773
 FORMAT = "utf8"
-
-def sendList(client, list):
-    for item in list:
-        client.sendall(item.encode(FORMAT))
-        #wait response
-        client.recv(1024)
-        
-    msg = "end"
-    client.send(msg.encode(FORMAT))
+BUFFER_SIZE = 1024
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -22,21 +14,37 @@ try:
     client.connect((HOST, SERVER_PORT))
     print("Client adress: ", client.getsockname())
     
-    list = ["thong", "20", "hello"]
-    
     msg = None
     while (msg != "x"):
         msg = input("talk: ")
         client.sendall(msg.encode(FORMAT))
         #gửi dữ liệu phức tạp
-        if (msg == "sendList"):
+        if (msg == "send file"):
             #wait response
-            client.recv(1024)
-            sendList(client, list)
+            response = client.recv(BUFFER_SIZE).decode(FORMAT)
+            
+            #upload file
+            if (response == "OK, send it!"):
+                fileName = input('The file name of file you want to upload: ') #D:\000MINHTHONG\Năm 2\Vovab\vocab01.txt
+                client.sendall(fileName.encode(FORMAT))
+                
+                client.recv(BUFFER_SIZE).decode(FORMAT)
+            
+                try:
+                    with open(fileName, "rb") as fi:
+                        data = fi.read(BUFFER_SIZE)
+                        while data:
+                            client.sendall(data)
+                            data = fi.read(BUFFER_SIZE)
+                            
+                    client.sendall(b"END")
+                except:
+                    print("File is error to read! Please try again")
+              
         
 except:
     print("Error!")
     
-input()
+input("Press any key to close client!")
 
 client.close()
--- socketcheck1/socketcheck1/server.py
+++ socketcheck1/socketcheck1/server.py
@@ -5,34 +5,47 @@ HOST = "127.0.0.1"
 # IP = "192.168.1.60"
 SERVER_PORT = 58773
 FORMAT = "utf8"
+BUFFER_SIZE = 1024 
 
-def recvList(conn):
-    list = []
-    item = None
-    while (item != "end"):
-        item = conn.recv(1024).decode(FORMAT)
-        list.append(item)
-        #response
-        conn.sendall(item.encode(FORMAT))
-        
-    return list
-
-def handleClient(conn: socket, addr):
+def handleClient(conn: socket, addr, nClient):
     print("conn: ", conn.getsockname())
     
     msg = None
     while (msg != "x"):
-        msg = conn.recv(1024).decode(FORMAT)
+        msg = conn.recv(BUFFER_SIZE).decode(FORMAT)
         print("client ", addr, " says ", msg)
         
-        #gửi dữ liệu phức tạp
-        if (msg == "sendList"):
+        #upload file
+        if(msg == "send file"):
             #response
-            conn.sendall(msg.encode(FORMAT))
-            list = recvList(conn)
+            conn.sendall("OK, send it!".encode(FORMAT))
             
-            print("Receive: ")
-            print(list)
+            fileNameInp = conn.recv(BUFFER_SIZE).decode(FORMAT);
+           
+            fileNameOut = ""
+            for i in range(len(fileNameInp) - 1, -1, -1):
+                if (fileNameInp[i] == "\\"):
+                    fileName = fileNameInp[i + 1:len(fileNameInp) - 1]
+                    fileNameOut = "output_" + fileName
+                    
+                    break
+                
+            destFolderPath = input('Enter the folder path you want to upload file: ') #D:\\000MINHTHONG\\Năm 2
+            destFilePath = destFolderPath + "\\" + fileNameOut 
+
+            conn.sendall(msg.encode(FORMAT))
+
+            with open(destFilePath, "wb") as fo:
+                while True:
+                    data = conn.recv(BUFFER_SIZE)
+                    if data == b"END":
+                        break;
+                    fo.write(data)
+                   
+            print() 
+            print('Receiving file from client', nClient)
+            print('Received successfully! New filename is:', fileNameOut)
+    
             
     print("client ", addr, " finished")
     print("client ", conn.getsockname(), " close")
@@ -54,9 +67,9 @@ while (nClient < 3):
     try:
         conn, addr = s.accept()
 
-        thr = threading.Thread(target = handleClient, args = (conn, addr))
+        thr = threading.Thread(target = handleClient, args = (conn, addr, nClient))
         #Nếu daemon = True và số client >= 3 thì server sẽ dừng và mặc kệ client1, client2 vẫn đang chạy,
-        #nếu daemon = False thì dù số client >= 3 thì server vẫn sẽ đợi cli1 và cl2 chạy xong rồi mới tắt
+        #nếu daemon = False thì dù số client >= 3 thì server vẫn sẽ đợi cli1 và cl2 chạy xong rồi mới tắt server    
         thr.daemon = False
         thr.start()
     
